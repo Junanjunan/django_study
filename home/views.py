9d117c72da8eec555d6ad4bfccac27b95ba6f7c1
Junanjunan <taltalrealty@gmail.com>
2023-01-23 15:29:47 +0900

    receive image file request and save in DB

--- home/views.py
+++ home/views.py
@@ -3,11 +3,14 @@ from home.models import Home
 
 
 def home(request):
-    banner_image = request.FILES.get('banner')
-    ext = str(banner_image).split(".")[-1]
-    image_name = f'new_name.{ext}'
-    home = Home.objects.create(title='test', banner=banner_image)
-    home.banner = banner_image
-    home.banner.name = image_name
-    home.save()
-    return render(request, 'home.html')
+    if request.method == 'POST':
+        banner_image = request.FILES.get('banner')
+        title = request.POST.get('title')
+        ext = str(banner_image).split(".")[-1]
+        image_name = f'new_name.{ext}'
+        home = Home.objects.create(title=title)
+        home.banner = banner_image
+        home.banner.name = image_name
+        home.save()
+    samples = Home.objects.all()
+    return render(request, 'home.html', {'samples':samples})
