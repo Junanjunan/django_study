26d3b4be8253b9d9537f3ebcbac9baf2384d856e
Junanjunan <taltalrealty@gmail.com>
2023-01-23 10:58:15 +0900

    Image upload - media

--- home/models.py
+++ home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+def custom_upload_to():
+    return datetime.now()
+
+class Home(models.Model):
+    title = models.CharField(max_length=30)
+    banner = models.ImageField(upload_to=custom_upload_to(), null=True)
--- home/views.py
+++ home/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from home.models import Home
 
 
 def home(request):
+    banner_image = request.FILES.get('banner')
+    ext = str(banner_image).split(".")[-1]
+    image_name = f'new_name.{ext}'
+    home = Home.objects.create(title='test', banner=banner_image)
+    home.banner = banner_image
+    home.banner.name = image_name
+    home.save()
     return render(request, 'home.html')
